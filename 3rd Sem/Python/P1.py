f84829abd175ad6e27e74a0670ea62f19ab84724
Shyam M <shyammanikam1904@gmail.com>
2024-01-01 23:45:20 +0530

    1st Program

---
+++ 3rd Sem/Python/P1.py
@@ -0,0 +1,10 @@
+a=10
+b='Python'
+c=10.5
+d=2.14
+e=True
+print('Datetype of variable a : ',type(a))
+print('Datetype of variable b : ',type(b))
+print('Datetype of variable c : ',type(c))
+print('Datetype of variable d : ',type(d))
+print('Datetype of variable e : ',type(e))
